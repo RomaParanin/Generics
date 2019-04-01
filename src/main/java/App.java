584f135c00fc1fcad2d3371cbbf6243bbda799e2
oceanwd <lighthouse163@gmail.com>
2019-04-01 11:09:26 +0400

    first

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,22 +14,23 @@ public class App {
         ServiceImpl service = new ServiceImpl();
         List<UserDTO> userDTOList = service.getUsers();
 
-        System.out.println("***** Добавление следующих пользователей в репозиторий *****");
+        System.out.println("***** Добавление следующих пользователей в репозиторий *****\n");
+
         service.saveUsers(userDTOList);
 
-        System.out.println("***** Сортировка репозитория по id пользователей в порядке возрастания *****");
+        System.out.println("\n***** Сортировка репозитория по id пользователей в порядке возрастания *****\n");
         Collections.sort(userDTOList, Comparator.comparing(UserDTO::getId));
         for (UserDTO userDTO : userDTOList){
             System.out.println(userDTO.toString());
         }
 
-        System.out.println("***** Сортировка репозитория по дате рождения в порядке убывания *****");
+        System.out.println("\n***** Сортировка репозитория по дате рождения в порядке убывания *****\n");
         Collections.sort(userDTOList, Comparator.comparing(UserDTO::getBirthDate).reversed());
         for (UserDTO userDTO : userDTOList){
             System.out.println(userDTO.toString());
         }
 
-        System.out.println("***** Сортировка репозитория по номеру квартир пользователей в порядке возрастания *****");
+        System.out.println("\n***** Сортировка репозитория по номеру квартир пользователей в порядке возрастания *****\n");
         Comparator<UserDTO> comparator = Comparator.comparing(apartment -> apartment.getAddress().getApartment());
         Collections.sort(userDTOList, comparator);
         for (UserDTO userDTO : userDTOList) {
